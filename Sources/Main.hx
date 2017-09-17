28773311499a4587e77e02c3d083fcd52c117eee
Robert Konrad <git2016@robdangero.us>
2017-09-17 12:15:46 +0200

    Kode Garden commit

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -18,7 +18,7 @@ class Main {
             // Avoid passing update/render directly so replacing
             // them via code injection works
 			Scheduler.addTimeTask(function () { update(); }, 0, 1 / 60);
-			System.notifyOnRender(function () { render(); });
+			System.notifyOnRender(function (framebuffer) { render(framebuffer); });
 		});
 	}
 }
