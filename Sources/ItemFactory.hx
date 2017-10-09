81eb37cf7ebc9370e177079331c7294848e6776f
Robert Konrad <git2016@robdangero.us>
2017-10-09 22:17:16 +0000

    Kode Garden commit

--- Sources/ItemFactory.hx
+++ Sources/ItemFactory.hx
@@ -1,1 +1,27 @@
 package;
+
+/**
+ * ...
+ * @author Joaquin
+ */
+class ItemFactory extends Entity
+{
+	private var mushrooms:Entity;
+	private var itemLayer:Layer;
+	public var collisions:CollisionGroup;
+	public function new(aLayer:Layer) 
+	{
+		super();
+		mushrooms = new Entity();
+		mushrooms.pool = true;
+		addChild(mushrooms);
+		collisions = new CollisionGroup();
+		itemLayer = aLayer;
+	}
+	public function createMushroom(aX:Float,aY:Float):Void
+	{
+		var mushroom:Mushroom = cast mushrooms.recycle(Mushroom);
+		mushroom.reset(aX, aY, itemLayer, collisions);
+	}
+	
+}
