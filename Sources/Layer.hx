deb3efb22ab42466e85a8cd0973b1a9d98d0a8db
Robert Konrad <git2016@robdangero.us>
2017-10-09 22:17:39 +0000

    Kode Garden commit

--- Sources/Layer.hx
+++ Sources/Layer.hx
@@ -1,1 +1,42 @@
 package;
+import kha.Canvas;
+
+/**
+ * ...
+ * @author Joaquin
+ */
+class Layer implements IDraw
+{
+	public var x:Float = 0;
+	public var y:Float = 0;
+	var mChildren:Array<IDraw>;
+	public var parent:Layer;
+	public function new() 
+	{
+		mChildren = new Array();
+	}
+	
+	public function add(aChild:IDraw)
+	{
+		mChildren.push(aChild);
+		aChild.parent = this;
+	}
+	
+	public function render(aFrameBuffer:Canvas,translation:Point):Void 
+	{
+		translation.x += x;
+		translation.y += y;
+		for (sprite in mChildren) 
+		{
+			sprite.render(aFrameBuffer,translation);
+		}
+		translation.x -= x;
+		translation.y -= y;
+	}
+	
+	public function remove(sprite:IDraw) 
+	{
+		mChildren.remove(sprite);
+	}
+	
+}
