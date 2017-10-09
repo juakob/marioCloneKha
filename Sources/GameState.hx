08ab04cba3788b94c688e66dd1140a1a399f5848
Robert Konrad <git2016@robdangero.us>
2017-10-09 22:14:43 +0000

    Kode Garden commit

--- Sources/GameState.hx
+++ Sources/GameState.hx
@@ -1,1 +1,48 @@
-package;
+package ;
+import kha.Canvas;
+
+/**
+ * ...
+ * @author joaquin
+ */
+class GameState extends Entity 
+{
+    var g4Renderer:G4Renderer;
+	var stage:Stage;
+	var pos:Point;
+	public function new() 
+	{
+        g4Renderer = new G4Renderer();
+		stage = new Stage();
+		pos = new Point();
+		super();
+	}
+	public function load(aResources:Resources):Void{
+		
+	}
+	public function init():Void {
+		
+	}
+	public function changeState(state:GameState):Void{
+		Simulation.i.changeState(state);
+	}
+	public function draw(aFramebuffer:Canvas):Void
+	{
+        g4Renderer.render(aFramebuffer);
+		aFramebuffer.g2.begin(false,stage.backgroundColor);
+        aFramebuffer.g2.imageScaleQuality = kha.graphics2.ImageScaleQuality.Low;
+		stage.render(aFramebuffer,pos);
+		aFramebuffer.g2.end();
+	}
+	
+	public function onActivate() 
+	{
+		
+	}
+	
+	public function onDesactivate() 
+	{
+		
+	}
+	
+}
