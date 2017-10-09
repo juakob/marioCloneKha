8a73680dc2746f309a152b710bd3c105b50553ed
Robert Konrad <git2016@robdangero.us>
2017-10-09 22:04:39 +0000

    Kode Garden commit

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -4,19 +4,26 @@ import kha.Framebuffer;
 import kha.Scheduler;
 import kha.System;
 
-class Main {
-	static function update(): Void {
 
+
+
+
+class Main {
+	static var simulation:Simulation;
+	static function update(): Void {
+		simulation.onEnterFrame();
 	}
 
 	static function render(framebuffer: Framebuffer): Void {
-
+		simulation.onRender(framebuffer);
 	}
-
+	
 	public static function main() {
-		System.init({title: "Kode Project", width: 800, height: 600}, function() {
+		System.init({title: "Kode Project", width: 800, height: 600,samplesPerPixel:2}, function() {
             // Avoid passing update/render directly so replacing
             // them via code injection works
+			simulation = new Simulation(Level);
+        //var t=new G();
 			Scheduler.addTimeTask(function () { update(); }, 0, 1 / 60);
 			System.notifyOnRender(function (framebuffer) { render(framebuffer); });
 		});
