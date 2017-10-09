1ec1cd7ad61236c83ac211bc6eaa50b0590bb9e1
Robert Konrad <git2016@robdangero.us>
2017-10-09 22:30:39 +0000

    Kode Garden commit

--- Shaders/default.frag.glsl
+++ Shaders/default.frag.glsl
@@ -1,3 +1,16 @@
+#version 450
+
+#ifdef GL_ES
+precision lowp float;
+#endif
+
+in vec2 fTexCoord;
+uniform sampler2D tex;
+
+out vec4 fragColor;
+
 void main() {
 
+	fragColor = texture(tex, fTexCoord) ;
+	
 }
