ab38d0ebfa4a3ab2e7ea2a70a72b8dc0e9449eef
Robert Konrad <git2016@robdangero.us>
2017-10-09 22:12:32 +0000

    Kode Garden commit

--- Sources/EnemySpawnPos.hx
+++ Sources/EnemySpawnPos.hx
@@ -1,1 +1,18 @@
 package;
+
+/**
+ * ...
+ * @author Joaquin
+ */
+
+class EnemySpawnPos
+{
+	public var x:Float;
+	public var y:Float;
+	public var type:EnemyType;
+		public function new() 
+		{
+			
+		}
+	
+}
