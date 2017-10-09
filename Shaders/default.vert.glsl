c377eee34c32bf9a0306ada94b5d2576c28fd7ec
Robert Konrad <git2016@robdangero.us>
2017-10-09 22:31:14 +0000

    Kode Garden commit

--- Shaders/default.vert.glsl
+++ Shaders/default.vert.glsl
@@ -1,3 +1,21 @@
-void main() {
+#version 450
 
+#ifdef GL_ES
+precision lowp float;
+#endif
+
+// Input vertex data, different for all executions of this shader
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec2 uv;
+layout(location = 2) in vec3 normal;
+
+out vec2 fTexCoord;
+
+uniform mat4 MVP;
+
+
+void main() {
+  vec4 npos = MVP * vec4(position,1.0);
+  fTexCoord = uv;
+  gl_Position =   npos;
 }
