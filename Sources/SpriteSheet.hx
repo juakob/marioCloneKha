981654f4d1fbb16afea9d3c5206cf7dd98d5c728
Robert Konrad <git2016@robdangero.us>
2017-10-09 22:26:03 +0000

    Kode Garden commit

--- Sources/SpriteSheet.hx
+++ Sources/SpriteSheet.hx
@@ -1,1 +1,43 @@
 package;
+import kha.Canvas;
+import kha.Framebuffer;
+
+/**
+ * ...
+ * @author Joaquin
+ */
+class SpriteSheet extends Sprite
+{
+	public var totalFrames(default, null):Int;
+	public var frame:Int=0;
+	var subX:Float=0;
+	var subY:Float = 0;
+	var rows:Int = 1;
+	var columns:Int = 1;
+	var separation:Int = 0;
+	
+	public function new(aImageName:String,aWidth:Float,aHeight:Float,aTotalFrames:Int=-1,aSeparation:Int=0) 
+	{
+		super(aImageName);
+		mWidth = aWidth;
+		mHeight = aHeight;
+		rows = Std.int(image.height / (aHeight+aSeparation*2));
+		columns = Std.int(image.width / (aWidth + aSeparation * 2));
+		trace((aWidth + separation * 2));
+		totalFrames = aTotalFrames;
+		if (aTotalFrames < 0)
+		{
+			aTotalFrames = rows * columns;
+		}
+		separation = aSeparation;
+		
+	}
+	override public function render(aFrameBuffer:Canvas,translation:Point):Void 
+	{
+		subX = (mWidth+separation*2) * (frame %columns);
+		subY = (mHeight+separation*2) * Std.int(frame / columns);
+		aFrameBuffer.g2.drawScaledSubImage(image,subX+separation,subY+separation,mWidth,mHeight, x+offsetX + translation.x, y+offsetY+translation.y,mWidth*scaleX,mHeight*scaleY);
+	}
+	
+	
+}
