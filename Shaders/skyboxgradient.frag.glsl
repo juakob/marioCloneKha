1b3cea171774d96b47e6191c8c8c0c7f9e07fee9
Robert Konrad <git2016@robdangero.us>
2017-10-09 22:31:54 +0000

    Kode Garden commit

--- Shaders/skyboxgradient.frag.glsl
+++ Shaders/skyboxgradient.frag.glsl
@@ -1,3 +1,21 @@
-void main() {
+#version 450
 
+#ifdef GL_ES
+    precision lowp float;
+#endif
+
+smooth in vec3 eyeDirection;
+
+out vec4 fragmentColor;
+
+uniform vec4 skytop = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+uniform vec4 skyhorizon = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+uniform int steps = 100;
+
+
+
+void main() {
+    float delta = eyeDirection.y;
+    vec4 mix =  mix(skyhorizon, skytop, delta);
+    fragmentColor =  (mix * steps)/steps;
 }
