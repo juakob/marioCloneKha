45bd753b27f8c164baf975f94debe6ed2ff515d2
Robert Konrad <git2016@robdangero.us>
2017-10-09 22:27:14 +0000

    Kode Garden commit

--- Sources/TimeManager.hx
+++ Sources/TimeManager.hx
@@ -1,1 +1,16 @@
 package;
+
+
+class TimeManager
+{
+
+	public static var delta(default, null):Float;
+	public static var time(default, null):Float=0;
+	
+	public static function setDelta(aDelta:Float):Void
+	{
+		delta = aDelta;
+		time+= aDelta;
+	}
+	
+}
