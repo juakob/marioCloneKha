0a0819333ce41cc1c3300276fcd08baad915eada
Robert Konrad <git2016@robdangero.us>
2017-10-09 22:19:21 +0000

    Kode Garden commit

--- Sources/Mesh.hx
+++ Sources/Mesh.hx
@@ -1,1 +1,20 @@
-package;
+package ;
+
+import kha.graphics4.IndexBuffer;
+import kha.graphics4.VertexBuffer;
+import kha.graphics4.VertexStructure;
+
+class Mesh{
+   
+    public var indices:IndexBuffer;
+    public var vertices:VertexBuffer;
+    public var vertexStructur:VertexStructure;
+    
+    public var count(get,null) : Int;
+    
+    public function new(){}
+    
+    function get_count() : Int{
+        return vertices.count();
+    }
+}
