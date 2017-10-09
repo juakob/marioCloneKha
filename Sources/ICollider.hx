7d9b0351a7fa295c8e81a52738f8f603df450899
Robert Konrad <git2016@robdangero.us>
2017-10-09 22:15:43 +0000

    Kode Garden commit

--- Sources/ICollider.hx
+++ Sources/ICollider.hx
@@ -1,1 +1,16 @@
 package;
+
+/**
+ * @author Joaquin
+ */
+
+interface ICollider 
+{
+	public function collide(aCollider:ICollider,?NotifyCallback:ICollider->ICollider->Void):Bool;
+	public function overlap(aCollider:ICollider,?NotifyCallback:ICollider->ICollider->Void):Bool;
+	public function collisionType():CollisionType;
+	public var userData:Dynamic;
+	public var parent:CollisionGroup;//TODO create ICollisionContainer
+	public function removeFromParent():Void;
+	
+}
