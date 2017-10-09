2cec20f8577cdef219174041a9163ac9d25373fc
Robert Konrad <git2016@robdangero.us>
2017-10-09 22:36:57 +0000

    Kode Garden commit

--- Sources/LevelData.hx
+++ Sources/LevelData.hx
@@ -1,1 +1,19 @@
 package;
+
+/**
+ * ...
+ * @author Joaquin
+ */
+class LevelData
+{
+
+	public static var map:CollisionTileMap;
+	public static var items:ItemFactory;
+	public static var camera:Camera;
+	public static function clear() {
+		map = null;
+		items = null;
+		camera = null;
+	}
+	
+}
