b184d2320dc7629fd0e0f9777c2b8e20551afdd2
Robert Konrad <git2016@robdangero.us>
2017-10-10 18:57:00 +0000

    Kode Garden commit

--- Sources/G4Renderer.hx
+++ Sources/G4Renderer.hx
@@ -1,178 +1,188 @@
 package;
 
-import kha.*;
+import kha.Canvas;
 import kha.graphics4.*;
 import kha.math.*;
 
-class BackgroundGenerator {
+class G4Renderer {
 
-    var plane:Node;
-    var frontLayer:Node;
-    var midLayer:Node;
-    var midBackLayer:Node;
-    var backLayer:Node;
+    public static var defaultStructure:VertexStructure;
 
-    var pillar:Mesh;
-    var ground:Mesh;
-    var mountain:Mesh;
-    var platform:Mesh;
-    var platformSlant:Mesh;
+    var root:Transform = new Transform();
+    public static var camera:Camera3D;
 
-    var ground_texture:Image;
-    var mountain_texture:Image;
-    var pillar_texture:Image;
-    var platform_texture:Image;
+    //------------------------------------------
+    //
+    var quadVBO:kha.graphics4.VertexBuffer;
+    var quadIBO:kha.graphics4.IndexBuffer;
 
-    var maxwidth:Float = 250;
-    var maxItems:Int = 5;
+    var skyboxPipe:PipelineState; 
 
-    var assetCount:Int = 2;
-    var doneLoading:Bool = false;
-    var gr:G4Renderer;
+    var iSkyTop:ConstantLocation;
+    var iSkyHorizon:ConstantLocation;
+    var iSkyboxSteps:ConstantLocation;
+    var iSkyboxInverseProj:ConstantLocation;
+    var iSkyboxModelView:ConstantLocation;
+    var skyboxSteps:Int = 30;
+    var skyboxTop:FastVector4 = new FastVector4(1.0,1.0,1.0,1.0);
+    var skyboxHorizon:FastVector4 = new FastVector4(0.7,0.7,8.0,1.0);
 
-    var frontProps:Array<Dynamic>;
-    var middleProps:Array<Dynamic>;
-    var backProps:Array<Dynamic>;
+    //-------------------------------------------
+    //
+    var defaultPipeline:PipelineState;
+    var iMVP:ConstantLocation;
+    var iTexture:TextureUnit;
+    var iLightDirection:ConstantLocation;
+    var lightDirection:FastVector3;
 
-    public function new(){
-        plane = new Node();
-        frontLayer = new Node();
-        frontLayer.setPosition(0,0,-5);
-        midLayer = new Node();
-        midLayer.setPosition(0,0,-10);
-        backLayer = new Node();
-        backLayer.setPosition(0,0,-49);
-        backLayer = new Node();
-        backLayer.setPosition(0,0,-70);
-    }
+   public function new() {
 
-    public function init(gr:G4Renderer): Void{
-        this.gr = gr;
-       // kha.Assets.loadImage("grass", onLoadedImg);
-      //  kha.Assets.loadImage("mountaingrass", onLoadedImg);
-      //  kha.Assets.loadImage("stone", onLoadedImg);
-      //  kha.Assets.loadImage("brownstripe", onLoadedImg);
+        camera = new Camera3D();
+        var width = kha.System.windowWidth(0);
+        var height = kha.System.windowHeight(0);
+        camera.setPerspective(45 * (Math.PI/180),width/height, 0.01, 10000);
+        camera.transform.setPosition(0,2.0,0);
+        setupPipelines();
+        createQuad();
 
-       // kha.Assets.loadBlob("ground_obj", onLoadedBlob);
-      //  kha.Assets.loadBlob("pillar_obj", onLoadedBlob);
-      //  kha.Assets.loadBlob("mountain_obj", onLoadedBlob);
-      //  kha.Assets.loadBlob("platform_obj", onLoadedBlob);
-      //  kha.Assets.loadBlob("platform_slant_obj", onLoadedBlob);
-    }
+        lightDirection = new FastVector3(0.1,-1,-1);
 
-    function onLoadedImg(i) : Void{
-        assetCount--;
-        if(assetCount == 0 && !doneLoading) 
-        {
-            doneLoading = true;
-            setAssets();
-        }
+        var bgg = new BackgroundGenerator();
+        bgg.init(this);
+   }
+
+   public function addNode(t:Node) {
+     root.addChild(t);
+   }
+
+   
+
+   function setupPipelines(){
+
+        var vs = new VertexStructure();
+        vs.add("vertexPosition", VertexData.Float4);
+    
+        skyboxPipe = new PipelineState();
+        skyboxPipe.inputLayout = [vs];
+        skyboxPipe.vertexShader = kha.Shaders.skyboxgradient_vert;
+        skyboxPipe.fragmentShader = kha.Shaders.skyboxgradient_frag;
+        skyboxPipe.depthWrite = true;
+
+        skyboxPipe.compile();
+
+        iSkyTop = skyboxPipe.getConstantLocation("skytop");
+        iSkyHorizon = skyboxPipe.getConstantLocation("skyhorizon");
+        iSkyboxSteps = skyboxPipe.getConstantLocation("steps");
+        iSkyboxInverseProj = skyboxPipe.getConstantLocation("inProjectionMatrix");
+        iSkyboxModelView = skyboxPipe.getConstantLocation("modelView");
+
+        defaultStructure = new VertexStructure();
+        defaultStructure.add("position", VertexData.Float3);
+        defaultStructure.add("uv", VertexData.Float2);
+        defaultStructure.add("normal", VertexData.Float3);
         
+        defaultPipeline = new PipelineState();
+        defaultPipeline.vertexShader = kha.Shaders.default_vert;
+        defaultPipeline.fragmentShader = kha.Shaders.default_frag;
+        defaultPipeline.depthWrite = true;
+        defaultPipeline.depthMode = CompareMode.LessEqual;
+        defaultPipeline.inputLayout = [defaultStructure];
+
+        defaultPipeline.compile();
+
+        iMVP = defaultPipeline.getConstantLocation("MVP");
+        iTexture = defaultPipeline.getTextureUnit("tex");
+        iLightDirection = defaultPipeline.getConstantLocation("LightDirection");
+
+   }
+
+   function createQuad(){
+
+            var vs = new VertexStructure();
+            vs.add("position", VertexData.Float4);
+            
+            quadVBO = new VertexBuffer(4,vs,Usage.StaticUsage);
+            var data = quadVBO.lock();
+            data.set(0,-1);// x
+            data.set(1,-1);// y
+            data.set(2,1); // z
+            data.set(3,1); // w
+            //
+            //
+            data.set(4,1); // x
+            data.set(5,-1);// y
+            data.set(6,1); // z
+            data.set(7,1); // w
+            //
+            //
+            data.set(8,1); // x
+            data.set(9,1); // y
+            data.set(10,1);// z            
+            data.set(11,1);// w
+            //
+            //
+            data.set(12,-1);// x
+            data.set(13,1); // y
+            data.set(14,1); // z            
+            data.set(15,1); // w
+            
+            quadVBO.unlock();
+
+            quadIBO = new IndexBuffer(6, Usage.StaticUsage);
+            var idata = quadIBO.lock();
+            idata[0] = 0;
+            idata[1] = 1;
+            idata[2] = 2;
+            idata[3] = 2;
+            idata[4] = 3;
+            idata[5] = 0;
+            quadIBO.unlock();
+
     }
 
-    function onLoadedBlob(b) : Void{
-        assetCount--;
-        if(assetCount == 0 && !doneLoading) 
-        {
-            doneLoading = true;
-            setAssets();
+    public function render(buffer:Canvas){
+            buffer.g4.begin();
+            buffer.g4.clear(camera.clearColor,1.0,1);
+           renderSky(buffer);
+            renderTree(buffer);
+            buffer.g4.end();
+    }
+
+    function renderTree(buffer:Canvas){
+     buffer.g4.setPipeline(defaultPipeline);
+     buffer.g4.setVector3(iLightDirection, lightDirection);
+     
+        for(i in 0...root.childCount){
+            var node = root.getChild(i);
+            for(index in 0...node.childCount){
+                var child = cast(node.getChild(index), Node);
+                if(child.mesh == null) continue;
+
+                var mvp = FastMatrix4.identity();
+                mvp.setFrom(mvp.multmat(camera.projectionMatrix));
+                mvp.setFrom(mvp.multmat(camera.viewMatrix));
+                mvp.setFrom(mvp.multmat(FastMatrix4.fromMatrix4(child.getMatrix())));
+
+                if(child.texture != null)
+                buffer.g4.setTexture(iTexture, child.texture);
+                buffer.g4.setMatrix(iMVP, mvp);
+
+                buffer.g4.setIndexBuffer(child.mesh.indices);
+                buffer.g4.setVertexBuffer(child.mesh.vertices);
+                buffer.g4.drawIndexedVertices();
+            }
         }
     }
 
-    function setAssets(){
-
-        if(ground_texture != null) return;
-
-        gr.addNode(plane);
-        gr.addNode(frontLayer);
-        gr.addNode(midLayer);
-        gr.addNode(backLayer);
-
-        ground_texture = kha.Assets.images.grass;
-        //mountain_texture = kha.Assets.images.mountaingrass;
-       // pillar_texture = kha.Assets.images.stone;
-        //platform_texture = kha.Assets.images.brownstripe;
-            
-
-        ground = Meshparser.ObjToMesh(kha.Assets.blobs.ground_obj);
-    //    pillar = Meshparser.ObjToMesh(kha.Assets.blobs.pillar_obj);
-    //    mountain = Meshparser.ObjToMesh(kha.Assets.blobs.mountain_obj);
-     //   platform = Meshparser.ObjToMesh(kha.Assets.blobs.platform_obj);
-     //   platformSlant = Meshparser.ObjToMesh(kha.Assets.blobs.platform_slant_obj);
-
-        frontProps = [platform, platform_texture, platformSlant, platform_texture];
-        middleProps = [pillar, pillar_texture];
-        backProps = [mountain, mountain_texture];
-
-        generate();
+    function renderSky(buffer:Canvas) {
+        buffer.g4.setPipeline(skyboxPipe);
+        buffer.g4.setMatrix(iSkyboxInverseProj, camera.projectionMatrix.inverse());
+        buffer.g4.setMatrix(iSkyboxModelView, camera.viewMatrix);
+        buffer.g4.setVector4(iSkyTop, skyboxTop);
+        buffer.g4.setVector4(iSkyHorizon, skyboxHorizon);
+        buffer.g4.setInt(iSkyboxSteps, skyboxSteps);
+        buffer.g4.setIndexBuffer(quadIBO);
+		buffer.g4.setVertexBuffer(quadVBO);
+        buffer.g4.drawIndexedVertices();
     }
-
-    public function generate(): Void{
-        var node = new Node();
-        node.texture = ground_texture;
-        node.mesh = ground;
-        node.setPosition(0,0,-100);
-        frontLayer.addChild(node);
-        //for(i in 0...maxItems){
-            //frontLayer.addChild(getProp(0));
-            //frontLayer.addChild(getProp(0));
-            //frontLayer.addChild(getProp(0));
-            //frontLayer.addChild(getProp(0));
-            //frontLayer.addChild(getProp(0));
-            //frontLayer.addChild(getProp(0));
-            //frontLayer.addChild(getProp(0));
-            //frontLayer.addChild(getProp(0));
-            //frontLayer.addChild(getProp(0));
-            //frontLayer.addChild(getProp(0));
-            //frontLayer.addChild(getProp(1));
-            //frontLayer.addChild(getProp(2));
-            //frontLayer.addChild(getProp(3));
-            //frontLayer.addChild(getProp(3));
-        //}
-        trace("Generated");
-    }
-
-    function getProp(layer:Int) : Node{
-        var n = new Node();
-        var rand:Int = 0;
-        switch(layer){
-            case 0:
-                var length =Std.int( frontProps.length/2 );
-                rand = Std.int(Math.random() * length);
-                n.mesh = frontProps[rand*length];
-                n.texture = frontProps[rand*length+1];
-                n.setLocalPosition(Math.random()*maxwidth-50,-Math.random(),Math.random());
-
-             case 1:
-                var scale = Math.random()*12+1;             
-                var length =Std.int( middleProps.length/2 );
-                rand = Std.int(Math.random() * length);
-                n.mesh = middleProps[rand*length];
-                n.texture = middleProps[rand*length+1];
-                n.setScale(scale, scale, scale);
-                n.setLocalPosition(Math.random()*maxwidth-50,0,Math.random());
-            
-            case 2:
-                var length =Std.int( middleProps.length/2 );
-                var scale = Math.random()*12+1;
-                rand = Std.int(Math.random() * length);
-                n.mesh = middleProps[rand*length];
-                n.texture = middleProps[rand*length+1];
-                n.setLocalPosition(Math.random()*maxwidth-50,-2,Math.random());
-                //n.setScale(scale, scale, scale);
-
-            case 3:
-                var length =Std.int( backProps.length/2 );
-                var scale = Math.random()*10+1;
-                rand = Std.int(Math.random() * length);
-                n.mesh = backProps[rand*length];
-               n.setScale(scale, scale, scale);
-                n.texture = backProps[rand*length+1];
-                n.setLocalPosition(Math.random()*maxwidth*2-200,0,Math.random());
-                
-        }
-
-        return n;
-    }
-
 }
